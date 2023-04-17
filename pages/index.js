a15eea205513e48e81a998e92b4c29b4bd06b8c1
DarkAkeno <darkakenopriestess@gmail.com>
2023-04-17 13:30:49 +0100

    Improved Changes

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
 
 
-
+  const skills = ['HTML5', 'CSS3', 'TailwindCSS', 'NextJs', 'NodeJS', 'JavaScript', 'Java', 'ReactJS', 'MongoDB', 'SQL',]
 
 
   return (
@@ -36,40 +36,35 @@ export default function Home() {
           <p className='m-0 text-white md:text-3xl text-xl text-center '>
             I am a software engineering student and aspiring web developer
           </p>
-          <button className="bg-[#BE93D4] hover:bg-purple-600 transition-colors duration-300 text-white font-bold py-2 px-4 rounded inline-flex mt-4 items-center">
+          <a href="CV.docx" download={"CV.docx"} className="bg-[#BE93D4] hover:bg-purple-600 transition-colors duration-300 text-white font-bold py-2 px-4 w- rounded inline-flex mt-4 items-center">
             <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
             <span>Download CV</span>
-          </button>
+          </a>
         </div>
       </section>
       <Wave fill={"BE93D4"} bg={"9867C5"} />
-      <section id="about" className='min-h-fit section-padding bg-[#BE93D4]'>
+      <section id="about" className='min-h-fit bg-[#BE93D4]'>
         <div className='container'>
           <h1 className='text-5xl text-white p-2'>About Me</h1>
           <p className='text-white p-2'>Hello, I&apos;m Michael Connor. I&apos;m a full time university student studying Software Engineering and also study web development on the side. I have a strong passion for both software and website development, and currently have experience creating both software and websites using JavaScript / NextJs / Reactjs / Java and some other frameworks</p>
           <h1 className='text-white p-2 text-5xl'>Technologies I&apos;ve worked with:</h1>
           <ul className='flex flex-wrap gap-4 my-3 p-2 text-white'>
-            <li >HTML5</li>
-            <li>CSS3</li>
-            <li>TailwindCSS</li>
-            <li>NextJs</li>
-            <li>NodeJS</li>
-            <li>JavaScript</li>
-            <li>Java</li>
-            <li>ReactJS</li>
-            <li>MongoDB</li>
-            <li>SQL</li>
-
+            {skills.map((skill, i) => (
+              <li key={i}>{skill}</li>
+            ))}
+          </ul>
+          <h1 className='text-white p-2 text-5xl'>Certificates</h1>
+          <h2 className='text-white pl-2 text-lg'> LinkedIn Learning</h2>
+          <ul className='flex flex-col gap-2 pl-2 mt-1 list-disc text-white'>
+            <li className='ml-10'>Efficient Time Management</li>
+            <li className='ml-10'>Project Management Simplified</li>
+          </ul>
+          <h2 className='text-white pl-2 mt-6 text-lg'>Languages</h2>
+          <ul className='flex flex-col gap-2 pl-2 mt-1 list-disc text-white'>
+            <li className='ml-10'>English - Fluent (Native) </li>
+            <li className='ml-10'>Japanese - A1</li>
           </ul>
         </div>
-        {/* <div className='m-auto max-w-7xl gap-5 grid sm:grid-cols-12 grid-cols-1 container sm:grid-rows-none grid-rows-1'>
-          <p className='sm:col-span-6 col-span-12'>
-            Image
-          </p>
-          <p className='sm:col-span-6 col-span-12'>
-            dummy text
-          </p>
-        </div> */}
       </section>
       <Wave fill={"9867C5"} bg={"BE93D4"} />
       <Card />
